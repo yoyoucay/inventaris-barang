57f342f6288ef45cb36a2f8a261174f7acd513e7
yoyoucay <44655602+yoyoucay@users.noreply.github.com>
2025-03-09 18:48:54 +0700

    feat: sorting data for report based on Year, Semester, Kode barang

--- app/reports/page.tsx
+++ app/reports/page.tsx
@@ -7,6 +7,7 @@ import PageLayout from '@/components/shared/PageLayout';
 import Loading from '@/components/shared/Loading';
 import { PDFDownloadLink } from '@react-pdf/renderer';
 import PdfDocument from '@/components/shared/PdfDocument';
+import Card from '@/components/shared/Card';
 
 export default function Reports() {
     const [pageState, setPageState] = useState<number>(0);
@@ -18,15 +19,22 @@ export default function Reports() {
         const response = httpGet('/api/report');
         const data: any = await response;
         console.log('data report : ', data)
-        setRowData(data.map((item: any) => ({
+        setRowData(data.sort((a: any, b: any) => {
+            if (a.iYear !== b.iYear) {
+                return a.iYear - b.iYear;
+            }
+            if (a.iSemester !== b.iSemester) {
+                return a.iSemester - b.iSemester;
+            }
+            return a.sKode.localeCompare(b.sKode);
+        }).map((item: any) => ({
             "Kode": item.sKode,
             "Nama": item.sName,
             "Tipe": item.iType,
-            "Satuan": item.sUoM,
             "Semester": item.iSemester,
             "Tahun": item.iYear,
             "Baik": item.iCondition1,
-            "Kurang Baik": item.iCondition2,
+            "Krg. Baik": item.iCondition2,
             "Rusak": item.iCondition3,
             "Deskripsi": item.sDesc
         })));
@@ -42,14 +50,17 @@ export default function Reports() {
             <div className="p-2">
                 <main className="p-4 mt-2">
                     <div className="col-span-12 space-y-4">
-                        <div className="mb-4">
-                            <PDFDownloadLink
-                                document={<PdfDocument title="Laporan Inventaris SMP Negeri 8 Batam" data={rowData} splitBy={['Tahun', 'Semester']} />}
-                                fileName="report.pdf"
-                                className="px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600"
-                            >
-                                {({ loading }) => (loading ? 'Generating PDF...' : 'Download PDF')}
-                            </PDFDownloadLink>
+                        <div className='grid grid-cols-1 gap-4'>
+                            <Card>
+                                <PDFDownloadLink
+                                    target="_blank"
+                                    document={<PdfDocument title="Laporan Inventaris SMP Negeri 8 Batam" data={rowData} splitBy={['Tahun', 'Semester']} />}
+                                    fileName="report.pdf"
+                                    className="px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600"
+                                >
+                                    {({ loading }) => (loading ? 'Generating PDF...' : 'Preview Report')}
+                                </PDFDownloadLink>
+                            </Card>
                         </div>
                     </div>
                 </main>
